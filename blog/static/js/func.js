783c20badff37571108af3d9b931782d6a6ed411
jhao104 <946150454@qq.com>
2016-11-07 22:02:09 +0800

    [update] 添加回到顶部功能

--- blog/static/js/func.js
+++ blog/static/js/func.js
@@ -2,6 +2,10 @@
  * Created by Administrator on 2016/11/5.
  */
 
+
+/**
+ * 自动生成目录
+ */
 function GenerateContentList() {
     $(".post-content").find("h2").each(function (i, item) {
         var tag = $(item).get(0).localName;
@@ -23,6 +27,32 @@ function GenerateContentList() {
             $(this).attr({"title": "收起"});
         }
         $("#AnchorContent").toggle();
+    });
+}
+
+/**
+ * 回到顶部功能.
+ */
+function backToTop() {
+    $(document).ready(function () {
+        //首先将#back-to-top隐藏
+        $("#back-to-top").hide();
+        //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+        $(function () {
+            $(window).scroll(function () {
+                if ($(window).scrollTop() > 500) {
+                    $("#back-to-top").fadeIn(1500);
+                }
+                else {
+                    $("#back-to-top").fadeOut(1500);
+                }
+            });
+            //当点击跳转链接后，回到页面顶部位置
+            $("#back-to-top").click(function () {
+                $('body,html').animate({scrollTop: 0}, 1000);
+                return false;
+            });
+        });
     });
 }
 

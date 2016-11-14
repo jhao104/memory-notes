1ef198951d14ba552e25b017c2ccf1d33561b988
jhao104 <946150454@qq.com>
2016-11-14 13:45:53 +0800

    add this 2016.11.14

---
+++ Pycharm/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     __init__.py.py  
+   Description :  
+   Author :       JHao
+   date：          2016/11/14
+-------------------------------------------------
+   Change Activity:
+                   2016/11/14: 
+-------------------------------------------------
+"""
+__author__ = 'JHao'
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if __name__ == '__main__':
+    pass

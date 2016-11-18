76267565ef4c1700abf376ad2540fae117728552
jhao104 <946150454@qq.com>
2016-11-18 22:23:56 +0800

    [update] add view number

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,1 +1,32 @@
-# -*- coding: UTF-8 -*-from django.shortcuts import render, get_object_or_404, redirect, render_to_responsefrom django.http import Http404, HttpResponsefrom article.models import Article# Create your views here.def home(request):    post_list = Article.objects.all()  # 获取全部文章    return render(request, 'article/articles.html', {"post_list": post_list})def detail(request, id):    post = get_object_or_404(Article, pk=id)    return render(request, 'article/article.html', {"post": post})
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     views.py  
+   Description :  
+   Author :       JHao
+   date：          2016/11/18
+-------------------------------------------------
+   Change Activity:
+                   2016/11/18: 
+-------------------------------------------------
+"""
+
+# -*- coding: UTF-8 -*-
+
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+# from django.http import Http404, HttpResponse
+
+from article.models import Article
+
+
+# Create your views here.
+
+def home(request):
+    post_list = Article.objects.all()  # 获取全部文章
+    return render(request, 'article/articles.html', {"post_list": post_list})
+
+
+def detail(request, id):
+    post = get_object_or_404(Article, pk=id)
+    post.viewed()
+    return render(request, 'article/article.html', {"post": post})

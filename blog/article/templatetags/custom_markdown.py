ed3ef0b70d6dd53bd273f7101b0948a17ba4585f
jhao104 <946150454@qq.com>
2016-11-18 20:17:36 +0800

    [fix] 提取markdown中的主要

--- blog/article/templatetags/custom_markdown.py
+++ blog/article/templatetags/custom_markdown.py
@@ -1,40 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
--------------------------------------------------
-   File Name：     custom_markdown.py  
-   Description :  
-   Author :       JHao
-   date：          2016/10/11
--------------------------------------------------
-   Change Activity:
-                   2016/10/11: 
--------------------------------------------------
-"""
-__author__ = 'JHao'
-
-import markdown
-
-from django import template
-from django.template.defaultfilters import stringfilter
-from django.utils.safestring import mark_safe
-
-register = template.Library()
-
-
-@register.filter(is_safe=True)
-@stringfilter
-def custom_markdown(value):
-    return mark_safe(
-            markdown.markdown(value, extensions=['markdown.extensions.fenced_code',
-                                                 'markdown.extensions.codehilite',
-                                                 'markdown.extensions.tables'],
-                              safe_mode=True, enable_attributes=False))
-
-
-@register.filter(is_safe=True)
-@stringfilter
-def abstract_content(value):
-    import re
-    value = ' '.join(re.findall(u'\s*(.*?)\s*```.*?```', value, re.DOTALL))  # 去掉markdown代码块
-    value = re.sub(r'##|###|\s*', '', value)
-    return value
+# -*- coding: utf-8 -*-"""-------------------------------------------------   File Name：     custom_markdown.py   Description :   Author :       JHao   date：          2016/10/11-------------------------------------------------   Change Activity:                   2016/10/11:-------------------------------------------------"""__author__ = 'JHao'import markdownfrom django import templatefrom django.template.defaultfilters import stringfilterfrom django.utils.safestring import mark_saferegister = template.Library()@register.filter(is_safe=True)@stringfilterdef custom_markdown(value):    return mark_safe(            markdown.markdown(value, extensions=['markdown.extensions.fenced_code',                                                 'markdown.extensions.codehilite',                                                 'markdown.extensions.tables'],                              safe_mode=True, enable_attributes=False))@register.filter(is_safe=True)@stringfilterdef abstract_content(value):    import re    raw = value    value = ' '.join(re.findall(u'\s*(.*?)\s*```.*?```', value, re.DOTALL))  # 去掉markdown代码块    value = re.sub(r'##|###|\s*', '', value)    return raw if not value else value

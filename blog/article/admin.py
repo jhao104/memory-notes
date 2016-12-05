240064780a7a94db0c8c7e09fd217a919d0b9b57
jhao104 <946150454@qq.com>
2016-12-05 19:49:42 +0800

    [update] 添加admin

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 
 # Register your models here.
+
+from .models import Article, Category
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_time'
+    list_display = ('title', 'category', 'author', 'date_time', 'view')
+    list_filter = ('category', 'author')

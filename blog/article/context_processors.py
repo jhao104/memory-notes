5feb9bf14585be46e0d38004b3334432edaaf8eb
jhao104 <946150454@qq.com>
2016-12-15 21:40:12 +0800

    [update] 添加分类数量

--- blog/article/context_processors.py
+++ blog/article/context_processors.py
@@ -10,12 +10,30 @@
                    2016/11/19: 
 -------------------------------------------------
 """
+
+from django.db.models import Count
 from .models import Article, Category
 
 
+# def sidebar(request):
+#     rank_article = Article.objects.order_by("-view")[0:7]
+#     category = Category.objects.all()
+#     return {
+#         "rank_article": rank_article,
+#         "category": category,
+#     }
+
+
 def sidebar(request):
-    rank_article = Article.objects.order_by("-view")[0:7]
-    category = Category.objects.all()
+    article = Article.objects.all().order_by("-view")
+    article_list = article[:]
+    rank_article = article_list[0:7]
+    stat_dict = dict()
+    pk_dict = dict()
+    for tag in article_list:
+        stat_dict[tag.category.name] = stat_dict.get(tag.category.name, 0) + 1
+        pk_dict[tag.category.name] = tag.pk
+    category = [{'pk': pk_dict.get(name), 'name': name, 'count': stat_dict.get(name, 0)} for name in stat_dict]
     return {
         "rank_article": rank_article,
         "category": category,

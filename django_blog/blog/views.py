d73e44c98d8f3ddf47fd031361b1cfa7b719091b
jhao104 <946150454@qq.com>
2017-04-14 11:18:12 +0800

    采用网易云跟帖

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-
+# -*- coding: utf-8 -*-
 # Create your views here.
 
 from django.shortcuts import render
 
 
 def Index(requests):
-    return render(requests, 'blog/index.html')
+    return render(requests, 'blog/index.html', {"html_title": u"博客首页",
+                                                "source_id": "index"})
 
 
 def About(request):

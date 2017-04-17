e5e9a65f2dab9483d693381a8eac45b2a4c6eb48
jhao104 <946150454@qq.com>
2017-04-17 21:16:50 +0800

    [update] 添加文章排行

--- django_blog/blog/context_processors.py
+++ django_blog/blog/context_processors.py
@@ -12,13 +12,20 @@
 """
 __author__ = 'JHao'
 
-from blog.models import Category
+from blog.models import Category, Article
 
 
 def sidebar(request):
     category_list = Category.objects.all()
+    # 所有类型
+
+    article_rank = Article.objects.all().order_by('-view')
+    if len(article_rank) >= 6:
+        article_rank = article_rank[0:6]
     return {
-        'category_list': category_list
+        'category_list': category_list,
+        'article_rank': article_rank
+
     }
 
 
